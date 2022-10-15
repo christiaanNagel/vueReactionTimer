fb9ee5e2fcd52c0fef5e406b149093d74e30fff5
christiaanNagel <61054009+christiaanNagel@users.noreply.github.com>
2022-10-15 12:28:19 +0200

    Add hooks and such to test vue language implementation

---
+++ src/components/Block.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="block" v-if="showBlock" @click="stopTimer">
+            Click me
+        </div>
+        <div v-else>
+            ..Loading
+        </div>
+        <h6></h6>
+    </div>
+</template>
+
+<script >
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        },
+    }
+}
+
+</script>
+
+<style  scoped>
+.block {
+    padding: 1rem;
+    background-color: green;
+    color: white;
+    border-radius: 4px;
+    width: 400px;
+    margin: auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Reaction time: {{ score }} ms</p>
+        <p class="rank"> {{ rank }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Best in the west"
+        }
+        else if (this.score < 500) {
+            this.rank = "Average joe"
+        }
+        else {
+            this.rank = "You sure you're not a sloth"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rank {
+    font-size: 1rem;
+    font-weight: bold;
+    color: black;
+}
+</style>
